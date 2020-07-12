c8e37a0fde5826ab0c3501abc0e2975de7256ed0
weiguanyu <Weiguanyu1>
2020-07-12 12:19:23 +0800

    迭代器模式

--- src/main/java/iterator/Test.java
+++ src/main/java/iterator/Test.java
@@ -1,6 +1,7 @@
 package iterator;
 
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,7 +29,27 @@ public class Test {
             String str = list.get(i);
         }
         long l2 = System.currentTimeMillis();
-        System.out.println("使用get(i)遍历集合所用时间："+(l2-l1)+"毫秒");
+        System.out.println("使用get(i)遍历LinkedList集合所用时间："+(l2-l1)+"毫秒");
+
+//        List<String> list1 = new ArrayList<String>();
+//        for (int i = 0; i < 100000; i++) {
+//            list1.add("test"+i);
+//        }
+//
+//        long l3 = System.currentTimeMillis();
+//        Iterator<String> it2 = list1.iterator();
+//        while (it2.hasNext()){
+//            String str = it2.next();
+//        }
+//        long l4 = System.currentTimeMillis();
+//        System.out.println("使用迭代器遍历ArrayList集合所用时间："+(l4-l3)+"毫秒");
+//
+//        int size1 = list1.size();
+//        for (int i = 0; i < size1; i++) {
+//            String str = list1.get(i);
+//        }
+//        long l5 = System.currentTimeMillis();
+//        System.out.println("使用get(i)遍历ArrayList集合所用时间："+(l5-l4)+"毫秒");
 
     }
 }
